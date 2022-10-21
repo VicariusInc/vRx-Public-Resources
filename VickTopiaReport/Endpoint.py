61faa5c5ce658aaa505cd0e7765a3cdf3790ac8f
joaldir <joaldir@gmail.com>
2022-10-21 14:54:09 -0300

    beta 0.2

--- VickTopiaReport/Endpoint.py
+++ VickTopiaReport/Endpoint.py
@@ -39,14 +39,17 @@ def getEndpoints(apikey,urldashboard,fr0m,siz3):
 
     try:
         response = requests.get(urldashboard + '/vicarius-external-data-api/endpoint/search', params=params, headers=headers)
-        parsed = json.loads(response.text)
+        parsed = json.loads(response.text)        
         
     except:
         print("something is wrong, will try again....")
 
     strEndpoints = ""
     for i in parsed['serverResponseObject']:
-        strEndpoints += (str(i['endpointId']) + "," + i['endpointName'] + "," + i['endpointHash'] +"\n")
+        #print(json.dumps(i,indent=2))
+        #print(i['endpointOperatingSystem']['operatingSystemName'])
+        operatingSystemName = (i['endpointOperatingSystem']['operatingSystemName'])
+        strEndpoints += (str(i['endpointId']) + "," + i['endpointName'] + "," + i['endpointHash'] + "," + operatingSystemName + "\n")
     
     return strEndpoints
 
@@ -176,4 +179,66 @@ def getEndpointScoresImpactRiskFactors(apikey,urldashboard,fr0m,siz3):
             strEndpointScoresImpactRiskFactors += (str(endpointId) + "," + endpointName + "," + riskFactorTerm + "," + str(riskFactorScore) + "\n")
 
     return strEndpointScoresImpactRiskFactors
+
+def getEndpointGroupsCount(apikey,urldashboard):
+
+    headers = {
+        'Accept': 'application/json',
+        'Vicarius-Token': apikey,
+    }
+
+    params = {
+        'from': 0,
+        'size': 1,
+    }
+
+    try:
+        response = requests.get(urldashboard + '/vicarius-external-data-api/organizationEndpointGroup/search', params=params, headers=headers)
+        parsed = json.loads(response.text)
+        responsecount = parsed['serverResponseCount']
+
+    except:
+        print('problem get number of groups')
+    
+    return responsecount
+
+    
+
+def getEndpointGroups(apikey,urldashboard,fr0m,siz3):
+
+    headers = {
+        'Accept': 'application/json',
+        'Vicarius-Token': apikey,
+    }
+
+    params = {
+        'from': fr0m,
+        'size': siz3,
+        'sort': '-organizationEndpointGroupUpdatedAt',
+    }
+
+    try: 
+        response = requests.get(urldashboard + '/vicarius-external-data-api/organizationEndpointGroup/search', params=params, headers=headers)
+        #print(response.text)
+        jresponse = json.loads(response.text)
+        #print(json.dumps(jresponse['serverResponseCount'],indent=2))
+    except:
+        print('Something is wrong to obtain grups')
+
+    strAttributes = ''
+    for i in jresponse['serverResponseObject']:        
+        if '=in=' in i['organizationEndpointGroupFilters']:
+            filtersTags = json.loads(i['organizationEndpointGroupFilters'])
+           
+            for j in filtersTags:
+                prepAttrib = str(j['fieldValues']['attributes']).replace("=in=",":")
+                prepAttrib = prepAttrib.replace("(","").replace(")","")
+                listAttrib = prepAttrib.split(",")
+
+                for attr in listAttrib:
+                    attr = attr.replace(":",",")
+                    strAttributes += i['organizationEndpointGroupName'] + "," + attr + "\n"
+            
+    return strAttributes
+                
 
